984e3e6ea77df95ed18022bc88866b2a51f666fa
bitcooker <dreamhighpassionhigh@gmail.com>
2023-05-08 12:00:25 +0800

    feat: connect nuxtjs to firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules : [
+        '@nuxtjs/axios',
+        [
+            '@nuxtjs/firebase',
+            {
+                config: {
+                    apiKey: 'AIzaSyCkaB_sAlFOu1kOs2UCAtknjdaI3-9pCYw',
+                    authDomain: 'challenge-nuxt-auth.firebaseapp.com',
+                    projectId: 'challenge-nuxt-auth',
+                    storageBucket: 'challenge-nuxt-auth.appspot.com',
+                    messagingSenderId: '681989257575',
+                    appId: '1:681989257575:web:cfac80593bc86c923d97c2',
+                },
+                 services: {
+                    auth: true
+                 }
+            }
+        ]
+    ]
 })
